22c2ef3509e2f74bb0b0fdec417c83f5ae50c3dc
Roey Mechrez <roimehrez@gmail.com>
2017-02-22 11:18:39 +0200

    solution

--- DDIS_solution/diversity/deformableDiversity.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "mex.h"
-
-namespace dis
-{
-	void DeformableDiversity(mxArray** outputs, int inputsElementCount, const mxArray** inputs, unsigned nthreads = 0);
-}
--- DDIS_solution/diversity/diversity.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "mex.h"
-
-namespace dis
-{
-	void WeightedDiversityCount(mxArray** outputs, int inputsElementCount, const mxArray** inputs);
-	void DiversityCount(mxArray** outputs, int inputsElementCount, const mxArray** inputs);
-}
